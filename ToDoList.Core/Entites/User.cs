193ce2b29f6d549088232bdb4f3e061e599ef4b1
v.guliyev <v.guliyev@VGULIYEV>
2022-10-29 15:25:59 +0300

    Commit_1

---
+++ ToDoList.Core/Entites/User.cs
@@ -0,0 +1,13 @@
+﻿namespace ToDoList.Core.Entites
+{
+    using System;
+
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ ToDoList.DataAccess/Entites/User.cs
@@ -0,0 +1,23 @@
+﻿namespace ToDoList.DataAccess.Entites
+{
+    using System;
+
+    public class User
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        public int Phone { get; set; }
+    }
+}
