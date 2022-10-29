35cf47a0975ae67419821e77735a8152dbd51cb1
v.guliyev <v.guliyev@VGULIYEV>
2022-10-29 16:09:21 +0300

    Added project_structure

---
+++ ToDoList.Core/Services/Contracts/IUserService.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDoList.Models.Business.Service.Interface
+{
+    using System.Collections.Generic;
+    using ToDoList.Core.Entites;
+
+    public interface IUserService
+    {
+           void Create(User user);
+
+           List<User> GetUsers();
+
+           User GetUserByEmail(string email);
+    }
+}
---
+++ ToDoList.Core/Services/Implementation/UserService.cs
@@ -0,0 +1,25 @@
+﻿namespace ToDoList.Models.Business.Service.Implementation
+{
+    using System.Collections.Generic;
+    using ToDoList.Core.Entites;
+    using ToDoList.Models.Business.Service.Interface;
+
+    public class UserService : IUserService
+    {
+
+        public void Create(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+       public List<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+       public User GetUserByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
