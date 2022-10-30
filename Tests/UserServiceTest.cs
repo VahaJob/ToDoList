42530dcc27a94ba11fd0e781734d602010c33744
v.guliyev <v.guliyev@VGULIYEV>
2022-10-30 13:20:30 +0300

    fix

---
+++ Tests/UserServiceTest.cs
@@ -0,0 +1,12 @@
+
+namespace ToDoList.Tests
+{
+    public class UserServiceTest
+    {
+        [Fact]
+        public void add_user_then_count_more_one()
+        {
+            var mock =  
+        }
+    }
+}
